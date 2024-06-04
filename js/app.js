3cfcf4414bfda2f6fe19494ecf761aa17f508bea
Mark <70575298+ChuckiCode@users.noreply.github.com>
2024-06-04 13:23:18 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,28 @@
+// Button elements
+const homeBtn_element = document.querySelector('.homeBtn');
+const rsvpBtn_element = document.querySelector('.rsvpBtn');
+const submitEvent_element = document.querySelector('#submitEvent');
+
+// Wrapper elements
+const homeWrapper_element = document.querySelector('.wrapper');
+const detailsWrapper_element = document.querySelector('.detailsWrapper');
+const formWrapper_element = document.querySelector('.formWrapper');
+
+// Events
+homeBtn_element.addEventListener('click', function () {
+  if (homeWrapper_element.classList.contains('active')) {
+    homeWrapper_element.classList.replace('active', 'deactive');
+    detailsWrapper_element.classList.replace('deactive', 'active');
+  }
+});
+
+rsvpBtn_element.addEventListener('click', function () {
+  if (
+    detailsWrapper_element.classList.contains('active') ||
+    homeBtn_element.classList.contains('active')
+  ) {
+    detailsWrapper_element.classList.replace('active', 'deactive');
+    homeWrapper_element.classList.replace('active', 'deactive');
+    formWrapper_element.classList.replace('deactive', 'active');
+  }
+});
